7f5d6e29ece8d17a9e031e0587bd02ed9319891e
城　健太郎 <ks19790209@gmai.com>
2018-03-17 14:13:11 +0900

    todos

---
+++ test/system/categolies_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class CategoliesTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit categolies_url
+  #
+  #   assert_selector "h1", text: "Categoly"
+  # end
+end
